42c7451e2a65499694df059d067ed664bd2250cc
olklymenko <o.klymenko.home@gmail.com>
2024-12-11 15:48:18 +0200

    replace output of the ResultModal with a Portal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
+import {createPortal} from 'react-dom';
 
 const ResultModal = forwardRef(({remainingTime, targetTime, onReset}, ref) => {
   const dialogRef = useRef();
@@ -15,15 +16,17 @@ const ResultModal = forwardRef(({remainingTime, targetTime, onReset}, ref) => {
   });
 
   return (
-    <dialog className="result-modal" ref={dialogRef} onClose={onReset}>
-      {userLost && <h2>You lost!</h2>}
-      {!userLost && <h2>Your score: {score}</h2>}
-      <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-      <form method="dialog" onSubmit={onReset}>
-        <button>Close</button>
-      </form>
-    </dialog>
+    createPortal(<dialog className="result-modal" ref={dialogRef} onClose={onReset}>
+        {userLost && <h2>You lost!</h2>}
+        {!userLost && <h2>Your score: {score}</h2>}
+        <p>The target time was <strong>{targetTime} seconds.</strong></p>
+        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+        <form method="dialog" onSubmit={onReset}>
+          <button>Close</button>
+        </form>
+      </dialog>,
+      document.getElementById('modal')
+    )
   )
 });
 
