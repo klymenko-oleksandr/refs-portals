d9e48be67bfd577eba8d479764bad690e1a03307
olklymenko <o.klymenko.home@gmail.com>
2024-12-11 15:40:18 +0200

    Enhancing the Demo App "Result Modal"

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,6 +4,7 @@ const ResultModal = forwardRef(({remainingTime, targetTime, onReset}, ref) => {
   const dialogRef = useRef();
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,8 +15,9 @@ const ResultModal = forwardRef(({remainingTime, targetTime, onReset}, ref) => {
   });
 
   return (
-    <dialog className="result-modal" ref={dialogRef}>
+    <dialog className="result-modal" ref={dialogRef} onClose={onReset}>
       {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
       <form method="dialog" onSubmit={onReset}>
